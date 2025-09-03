8b945c3a75834a4aa9092f969cd147b2ea502312
swallehrashid <swallehmtawazo@gmail.com>
2025-09-03 09:03:07 +0300

    adds and changes the employee object

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Samuel",
+    streetAddress: "152 Upperhill",
+};
+
+
+
+function updateEmployeeWithKeyAndValue(object, key , value) {
+    const newObject = {...object};
+    newObject[key] = value;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
